c0fe86389780629b90e9c586ff40061941d240f3
Julia Löw <julia.loew@gmx.net>
2024-06-24 16:56:04 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+
+const App = () => {
+  return (
+  <> 
+  <div className='text-2xl text-red-500 '>Personal Diary</div>
+  <button className="btn-secondary">Secondary</button>
+  </> )
+};
+
+export default App;
