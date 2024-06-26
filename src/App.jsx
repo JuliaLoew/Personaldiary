35c5729925f008401d30e8f29d40d57511c35637
konstantinminster <konstantin@minster.biz>
2024-06-26 11:49:10 +0200

    header created for now with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './index.css';
 import Diaries from './components/Diaries.jsx';
 import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
 //import Adddiarybtn from './components/Adddiarybtn.jsx';
 
 
 const App = () => {
   return (
   <> 
-  <div className='text-2xl text-red-500 '>Personal Diary</div>
-  <button className="btn-secondary">Secondary</button>
+  <Header />
   <Diaries />
   <Footer />
   </> )
