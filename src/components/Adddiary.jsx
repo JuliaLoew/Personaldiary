cac9feb9cc0db187af59dfa3d64d14002470cf44
Yongqi Zhang <zhangyongqi7@gmail.com>
2024-06-26 10:13:03 +0200

    add addDiariesToLocalStorage

--- src/components/Adddiary.jsx
+++ src/components/Adddiary.jsx
@@ -1,5 +1,42 @@
 import AddDiaryBtn from "./Adddiarybtn.jsx";
+import { useState } from "react";
+
+export function ExistingDiaryDataDialog() {
+ return (
+    <div className="text-white bg-black">
+        You have already written a diary for today. Please try again tomorrow
+    </div>
+ )
+}
 export default function AddDiary() {
+  const [diaries, setDiaries] = useState({
+    title: "",
+    imgUrl:"",
+    entry: "",
+  });
+  const handleReset = () => {setDiaries({title: "", imgUrl:"", entry: ""})};
+  
+  function addDiaryToLocalStorage(key, value) {
+    const diaryData = JSON.parse(localStorage.getItem(key)) || [];
+    const existingDiaryData = diaryData.find((diary) => diary.date === value.date);
+    if (!existingDiaryData) {
+        const newDiaryData = [...diaryData, value];
+        localStorage.setItem(key, JSON.stringify(newDiaryData));
+       
+    }else{
+        alert("You have already written a diary for today. Please try again tomorrow");
+    }
+  }
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        setDiaries({
+          ...diaries,
+          [e.target.name]: e.target.value,
+        });
+        addDiaryToLocalStorage("diaryData", diaries);
+        handleReset();
+        document.getElementById("my_modal_3").close();
+      };
   return (
     <>
       <AddDiaryBtn />
@@ -11,6 +48,8 @@ export default function AddDiary() {
                 type="text"
                 className="grow"
                 name="title"
+                value={diaries.title}
+                onChange={(e) => setDiaries({ ...diaries, title: e.target.value, id: Date.now(), date: new Date().toLocaleDateString()})} 
                 placeholder="Please write your Title here"
               />
             </label>{" "}
@@ -19,14 +58,18 @@ export default function AddDiary() {
               <input
                 type="text"
                 className="grow"
-                name="image"
+                name="imgUrl"
+                value={diaries.imgUrl}
+                onChange={(e) => setDiaries({ ...diaries, imgUrl: e.target.value })}
                 placeholder="Please paste the image URL here"
               />
             </label>{" "}
             <br />
             <label className="flex items-center">
               <textarea
-                name="content"
+                name="entry"
+                value={diaries.entry}
+                onChange={(e) => setDiaries({ ...diaries, entry: e.target.value })}
                 className="mb-4 textarea textarea-accent grow"
                 placeholder="Please write your diary here"
               ></textarea>
@@ -39,6 +82,7 @@ export default function AddDiary() {
             <button
               className="absolute btn btn-outline btn-accent bottom-4 right-6"
               type="submit"
+              onClick={handleSubmit}
             >
               Submit
             </button>
