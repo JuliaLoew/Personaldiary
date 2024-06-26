3b1dbe7e596f78d30a7d5761b8c9ebf8c3e1b9da
JuliaLoew <julia.loew@gmx.net>
2024-06-26 10:58:41 +0200

    Update 2 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './index.css';
-// import Diaries from './components/Diaries.jsx';
-// import Adddiarybtn from './components/Adddiarybtn.jsx';
-import Diarydetails from './components/Diarydetails.jsx';
+import Diaries from './components/Diaries.jsx';
+import Adddiarybtn from './components/Adddiarybtn.jsx';
 
 
 const App = () => {
   return (
   <> 
-  <div className='text-2xl md:text-4xl font-semibold text-center text-blue-900 p-4'>Personal Diary</div>
+  <div className='text-2xl text-red-500 '>Personal Diary</div>
+  <button className="btn-secondary">Secondary</button>
+  <Diaries />
   </> )
 };
 
