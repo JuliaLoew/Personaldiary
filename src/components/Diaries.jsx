8799260fa0d2e85223ac856bfc419a1b21c09174
konstantinminster <konstantin@minster.biz>
2024-06-27 05:43:52 +0200

    added a setInterval to check for new data + sorting

--- src/components/Diaries.jsx
+++ src/components/Diaries.jsx
@@ -1,30 +1,43 @@
 import Diary from "./Diary";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 
 const Diaries = () => {
-  const [diaries, setDiary] = useState(() => {
+
+  const [diaries, setDiary] = useState([]);
+
+  const getDiaryEntries = () => {
     const saved = localStorage.getItem("diaryData");
-    const initialValue = JSON.parse(saved);
-    //console.log(saved);
-    return initialValue || [];
-  });
+    if (saved) {
+      const parsedData = JSON.parse(saved);
+      const sortedData = parsedData.sort((a, b) => b.id - a.id); // sortiert nach id=timestamp
+      setDiary(sortedData);
+  }
+};
 
+useEffect(() => {
+  getDiaryEntries();
 
+  // Set up an interval to check for updates in localStorage
+  const intervalId = setInterval(() => {
+    getDiaryEntries();
+  }, 3000); // check every second
+
+  // Clean up the interval on component unmount
+  return () => clearInterval(intervalId);
+}, []);
 
   return (
     <div className="flex flex-wrap justify-center gap-10 p-10">
       {diaries.map((item) => (
-
-          <Diary
-            key={item.id}
-            item={item}
-            title={item.title}
-            entry={item.entry}
-            date={item.date}
-            img={item.imageURL}
-          />
-
-    
+        <Diary
+          key={item.id}
+          item={item}
+          title={item.title}
+          entry={item.entry}
+          date={item.date}
+          img={item.imageURL}
+        />
       ))}
     </div>
   );
